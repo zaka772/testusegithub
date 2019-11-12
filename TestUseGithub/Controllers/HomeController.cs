41d8f5bdc2a1632108a61ca23139ce1420c7b355
iffat <iffatkhan416@gmail.com>
2019-11-12 17:07:10 +0800

    test

--- TestUseGithub/Controllers/HomeController.cs
+++ TestUseGithub/Controllers/HomeController.cs
@@ -34,10 +34,12 @@ namespace TestUseGithub.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult display()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
